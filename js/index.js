beb5fe9395e23e647874a72d10c67e5a182e0f1d
quanghuy6698 <quanghuy6698@gmail.com>
2024-01-10 00:10:01 +0700

    [10/1/2024] dev

--- js/index.js
+++ js/index.js
@@ -7,10 +7,34 @@ const appUrls = {
     android: "https://play.google.com/store/apps/details?id=com.ironhuy.classifychallenge",
     ios: "",
   },
-  flappyTrader: {
-    android: "https://play.google.com/store/apps/details?id=com.ironhuy.flappyTrader",
+  watermelonGame: {
+    android: "https://play.google.com/store/apps/details?id=com.ironhuy.watermelonGame",
     ios: "",
   },
+};
+
+const tabs = [
+  {
+    id: "privacyPolicy",
+    display: "block",
+    tabId: "privacyPolicyTab",
+    contentId: "privacyPolicy",
+  },
+  {
+    id: "appList",
+    display: "flex",
+    tabId: "appListTab",
+    contentId: "appList",
+  },
+];
+
+const tabCssClass = {
+  active: "tabs__tab--active",
+  deactive: "tabs__tab",
+};
+
+window.onload = (ev) => {
+  this.selectTab("privacyPolicy");
 };
 
 /**
@@ -20,4 +44,23 @@ const appUrls = {
 function viewAppOnStore(store, appId) {
   let appUrl = appUrls[appId][store];
   window.open(appUrl, "_blank");
+}
+
+/**
+ * select a tab
+ * @param tabId refer to tab id
+ */
+function selectTab(tabId) {
+  tabs.forEach((tab) => {
+    const tabEl = document.getElementById(tab.tabId);
+    const tabContentEl = document.getElementById(tab.contentId);
+
+    if (tab.id == tabId) {
+      tabContentEl.style.display = tab.display;
+      tabEl.classList.add(tabCssClass.active);
+    } else {
+      tabContentEl.style.display = "none";
+      tabEl.classList.remove(tabCssClass.active);
+    }
+  });
 }
